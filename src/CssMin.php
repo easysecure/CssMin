71f0eeed5eced92d23f07819d3b84a099aa47791
easysecure <100680857+easysecure@users.noreply.github.com>
2024-10-07 16:10:47 +0300

    Update CssMin.php to support PHP 8.3

--- src/CssMin.php
+++ src/CssMin.php
@@ -4335,6 +4335,8 @@ class CssAtPageDeclarationToken extends aCssDeclarationToken
  */
 class CssAtMediaStartToken extends aCssAtBlockStartToken
 {
+	public $MediaTypes = [];
+	
 	/**
 	 * Sets the properties of the @media at-rule.
 	 *
